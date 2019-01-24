15ce4d1f25a30eae71d3ffa47a09608f6f99bbf8
Tomas Latal <latal.tomas@gmail.com>
2019-01-24 17:40:05 +0100

    Rename blocks to loos that underscore

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,16 @@
 import React from "react";
 
-import { Hero } from "Blocks/_Hero";
-import { Stats } from "Blocks/_Stats";
-import { Tiles } from "Blocks/_Tiles";
-import { CardsRow } from "Blocks/_CardsRow";
-import { Comments } from "Blocks/_Comments";
-import { NewsLetter } from "Blocks/_Newsletter";
+import { Hero } from "Blocks/Hero";
+import { Tiles } from "Blocks/Tiles";
 import { Layout } from "Layout/Layout";
 
 const IndexPage = () => (
   <div>
     <Layout>
       <Hero />
-      <Stats />
       <div className="uk-panel uk-light uk-margin-medium" />
       <hr className="uk-divider-icon" />
       <Tiles />
-      <div className="uk-panel uk-light uk-margin-medium" />
-      <CardsRow />
-      <div className="uk-panel uk-light uk-margin-medium" />
-      <Comments />
-      <div className="uk-panel uk-light uk-margin-medium" />
-      <NewsLetter />
     </Layout>
   </div>
 );
